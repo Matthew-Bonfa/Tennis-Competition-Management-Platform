dc4ea511ff81de9def4b04b59cad7fdde4ac0d6d
Matthew Bonfa <matthewbonfa@gmail.com>
2026-09-15 15:16:12 +1000

    feat: added endpoints for getting all teams and a team by id

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,9 +5,10 @@ import { AppService } from './app.service.js';
 import { PrismaModule } from './prisma/prisma.module.js';
 import { MatchesModule } from './matches/matches.module.js';
 import { SectionsModule } from './sections/sections.module.js';
+import { TeamsModule } from './teams/teams.module.js';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, MatchesModule, SectionsModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, MatchesModule, SectionsModule, TeamsModule],
   controllers: [AppController],
   providers: [AppService],
 })
